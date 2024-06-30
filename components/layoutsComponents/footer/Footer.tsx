c25908f6df9e80dd97999e7e28919c57fa1b4a10
Evgeny Dudin <Jackson55553@gmail.com>
2024-06-30 13:55:48 +0500

    refactor footer

--- components/layoutsComponents/footer/Footer.tsx
+++ components/layoutsComponents/footer/Footer.tsx
@@ -2,29 +2,29 @@ import React from 'react';
 import styles from '../../../styles/sass/_footer.module.scss';
 import { PiTelegramLogo } from 'react-icons/pi';
 // import { FaDiscord } from 'react-icons/fa';
-// import { FaXTwitter } from 'react-icons/fa6';
+import { FaXTwitter } from 'react-icons/fa6';
 import Link from 'next/link';
 
 const Footer = () => {
     return (
         <footer className={styles.footerContainer}>
             <div className={styles.footer}>
-                <p>Token For Ever | 2024 | </p>
                 <ul className={styles.linkCategory}>
                     <Link href={'https://t.me/tokenforever'} target="_blank">
                         <PiTelegramLogo />
                         <p>Telegram</p>
                     </Link>
-                    {/* <Link href={'/'} target="_blank">
+                    <Link href={'https://x.com/TokenForEverTFE'} target="_blank">
                         <FaXTwitter />
                         <p>Twitter</p>
                     </Link>
-                    <Link href={'/'} target="_blank">
+                    {/*  <Link href={'/'} target="_blank">
                         <FaDiscord />
                         <p>Discord</p>
-                    </Link> */}
+                        </Link> */}
                 </ul>
             </div>
+            <p>Token For Ever | 2024</p>
         </footer>
     );
 };
